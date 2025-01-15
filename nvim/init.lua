20fee62a03a586fb20fdd6a32b6faadab71a56a3
Benyamin Krisna <benkris@DN0a225168.SUNet>
2025-01-15 11:00:25 -0800

    copilot keybinding

--- nvim/init.lua
+++ nvim/init.lua
@@ -77,3 +77,7 @@ vim.cmd("let g:magma_automatically_open_output = v:false")
 vim.cmd("let g:magma_image_provider = 'ueberzug'")
 
 vim.opt.cursorline = true
+
+-- Remap Copilot accept function to Ctrl + l
+vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true })
+vim.g.copilot_no_tab_map = true
