6c7ed56ebb72d7162cc44c5ca34cf85e056e5de5
Benyamin Krisna <benkris@DNa1cc674.SUNet>
2025-06-18 00:41:24 -0700

    commenter

--- nvim/init.lua
+++ nvim/init.lua
@@ -81,3 +81,5 @@ vim.opt.cursorline = true
 -- Remap Copilot accept function to Ctrl + l
 vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true })
 vim.g.copilot_no_tab_map = true
+
+require('Comment').setup()
